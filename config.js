5ad6292daaf534ee49566418f4b64dcd5c0a6f1e
ctruong3q3d <ctruong3q3d@gmail.com>
2022-12-21 20:58:17 +0700

    upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = '22/12 bé Dũi cụa anh';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Gửi em iu cụa anh';
+const contentCard = 'Hum nay của 20 năm trước là ngày mà a chờ đợi đã lâu. Ngày mặt trời bé bỏng của anh ra đời, ngày mụt thiên thần bé bỏng dễ thưng đến với cuộc sống này và đến với anh. Chúc em iuu cụa anh mụt tuổi mới tràn ngập hạnh phúc bênh anh iu cụa em cũng như bạn bè nè. Chúc bé dũi học hành thuận lợi nữa nhoa nhoa, và đặc biệt là luôn luôn vui tơi iuu đời, mãi mãi bên a, iuu thưng a hơn nữa. Và ít quạo với a hơn nữa nhoa iuu iuu nhiều lúm chụt chụt chụt. <3 <3 <3 ';
+
+
+const giftImage = 'emiu.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
